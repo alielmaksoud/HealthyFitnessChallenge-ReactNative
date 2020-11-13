cfbfe73430e8b7b89ae06d13a978387ca9fd2ba2
eikyuu <v.duguet@laposte.net>
2020-11-13 09:44:08 +0100

    add textInput in configChallengeScreen

--- components/ConfigChallengeScreen.js
+++ components/ConfigChallengeScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {ImageBackground, Text} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 import styledBackgroundImage from '../styledComponents/styledBackgroundImage';
+import styledText from '../styledComponents/styledText';
 
 const ConfigChallengeScreen = (props) => {
   return (
     <ImageBackground
       source={require('../assets/images/backgroundImage.jpg')}
       style={styledBackgroundImage.image}>
-      <Text>Configurer votre challenge</Text>
+      <Text style={styledText.title}>Configurer votre challenge</Text>
+      <TextInput defaultValue="You can type in me"></TextInput>
     </ImageBackground>
   );
 };
