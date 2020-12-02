29ce16ec6bde876ede9a63c319d9a0ecf30bf8b1
eikyuu <v.duguet@laposte.net>
2020-12-02 21:58:13 +0100

    advance count days

--- components/MyChallengeScreen.js
+++ components/MyChallengeScreen.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
-import {ImageBackground, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ImageBackground, Text, View, TouchableOpacity} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
+import {min} from 'react-native-reanimated';
 import {connect} from 'react-redux';
 import styledBackgroundImage from '../styledComponents/styledBackgroundImage';
 import styledButton from '../styledComponents/styledButton';
@@ -9,13 +10,28 @@ import styledView from '../styledComponents/styledView';
 
 const MyChallengeScreen = ({configChallenge}) => {
   const [state, setstate] = useState(configChallenge);
-  console.log(Object.values(state));
+  const [count, setCount] = useState(0);
+
+  const minInteger = Object.values(state).map((item, index) => {
+    let maxInteger = parseInt(item.days, 10);
+    let minInteger = maxInteger - maxInteger;
+
+    return minInteger;
+  });
+
+  console.log(minInteger);
 
   const articlesJsx = Object.values(state).map((item, index) => (
-    <View style={styledView.viewTodo} key={index}>
+    <TouchableOpacity
+      activeOpacity={1}
+      style={styledView.viewTodo}
+      onPress={() => setCount(count + 1)}
+      key={index}>
       <Text style={styledText.textBlack}>{item.name}</Text>
-      <Text style={styledText.textGray}>{item.days} Jours</Text>
-    </View>
+      <Text style={styledText.textGray}>
+        {minInteger[index]} {count} Jours
+      </Text>
+    </TouchableOpacity>
   ));
 
   return (
