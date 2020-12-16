70e27c251b0a81a4107f4f17e25e2a63bbaa3fc0
eikyuu <vduguet@laposte.net>
2020-12-16 09:03:15 +0100

    start add drawer navigator

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,90 +1,14 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import ChallengeScreen from '../views/ChallengeScreen';
-import HomeScreen from '../views/HomeScreen';
-import ConfigChallengeScreen from '../views/ConfigChallengeScreen';
-import MyChallengeScreen from '../views/MyChallengeScreen';
-import DetailsChallengeScreen from '../views/DetailsChallengeScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import StackNavigator from './StackNavigator';
 
 const Navigation = (props) => {
-  const Stack = createStackNavigator();
+  const Drawer = createDrawerNavigator();
 
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: '',
-          headerStyle: {
-            backgroundColor: '#52B788',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-      <Stack.Screen
-        name="Challenge"
-        component={ChallengeScreen}
-        options={{
-          title: '',
-          headerStyle: {
-            backgroundColor: '#52B788',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-
-      <Stack.Screen
-        name="ConfigChallengeScreen"
-        component={ConfigChallengeScreen}
-        options={{
-          title: '',
-          headerStyle: {
-            backgroundColor: '#52B788',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-
-      <Stack.Screen
-        name="MyChallengeScreen"
-        component={MyChallengeScreen}
-        options={{
-          title: '',
-          headerStyle: {
-            backgroundColor: '#52B788',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-
-      <Stack.Screen
-        name="DetailsChallengeScreen"
-        component={DetailsChallengeScreen}
-        options={{
-          title: '',
-          headerStyle: {
-            backgroundColor: '#52B788',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-    </Stack.Navigator>
+    <Drawer.Navigator>
+      <Drawer.Screen name="stackNavigator" component={StackNavigator} />
+    </Drawer.Navigator>
   );
 };
 
---
+++ components/StackNavigator.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import ChallengeScreen from '../views/ChallengeScreen';
+import HomeScreen from '../views/HomeScreen';
+import ConfigChallengeScreen from '../views/ConfigChallengeScreen';
+import MyChallengeScreen from '../views/MyChallengeScreen';
+import DetailsChallengeScreen from '../views/DetailsChallengeScreen';
+
+const StackNavigator = (props) => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: '',
+          headerStyle: {
+            backgroundColor: '#52B788',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+      <Stack.Screen
+        name="Challenge"
+        component={ChallengeScreen}
+        options={{
+          title: '',
+          headerStyle: {
+            backgroundColor: '#52B788',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+
+      <Stack.Screen
+        name="ConfigChallengeScreen"
+        component={ConfigChallengeScreen}
+        options={{
+          title: '',
+          headerStyle: {
+            backgroundColor: '#52B788',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+
+      <Stack.Screen
+        name="MyChallengeScreen"
+        component={MyChallengeScreen}
+        options={{
+          title: '',
+          headerStyle: {
+            backgroundColor: '#52B788',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+
+      <Stack.Screen
+        name="DetailsChallengeScreen"
+        component={DetailsChallengeScreen}
+        options={{
+          title: '',
+          headerStyle: {
+            backgroundColor: '#52B788',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
